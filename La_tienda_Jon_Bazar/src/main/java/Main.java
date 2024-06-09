5bbe5fea5f624010d11c1edf65194d84ef6cae20
Mr-JonatanGomez <148049964+Mr-JonatanGomez@users.noreply.github.com>
2024-06-09 16:16:49 +0200

    empezando

--- La_tienda_Jon_Bazar/src/main/java/Main.java
+++ La_tienda_Jon_Bazar/src/main/java/Main.java
@@ -9,6 +9,7 @@ public class Main {
         try {
             System.out.println(connection.getCatalog());// solo por ver la conex, que ya existe la database
             // TODO: 09/06/2024 continuar creando la DBCONECT proximo dia seguimos
+            // que estoy mas que hartao con el GIT del huevo
         } catch (SQLException e) {
             System.err.println("Error ejecucion conexion en Main");
         }
